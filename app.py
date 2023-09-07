624202e07fcc7301a3ded46cabfef24ff8f3df47
Rumman <rummanfardeen4567@gmail.com>
2023-09-07 21:31:08 +0530

    :fire: Files modified

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# Database creation
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
